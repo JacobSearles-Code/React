9967ce4295da0c58b934e715907320dfcef2b973
Jacob Searles <jacobtsearles@outlook.com>
2026-06-25 15:22:13 -0400

    Added Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from 'react';
-import { HashRouter, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route, Link, useLocation, useNavigate, Navigate } from 'react-router-dom';
 
 import LoginForm from './pages/LoginForm.jsx';
 import ClockTime from './pages/timeclock.jsx';
 
 const App = () => {
+  const [logStatus, setLogStatus] = useState(false);
+
+  const handleLog = () => {
+      setLogStatus(false);    
+  }
+
   return (
     <HashRouter>
       <header>
         <h1 className="compName">Bright Path Employee Portal</h1>
         <nav className="navBar"> 
-          <Link to="/">Login</Link>
+          <Link className='navItem' to="/" onClick={handleLog}>{logStatus ? "Log out" : "Log in"}</Link>
         </nav>
       </header>
 
       <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/React/clock" element={<ClockTime />} />
+        <Route path="/" element={<LoginForm setLogStatus={setLogStatus} />} />
+        <Route path="/React/clock" element={ logStatus? <ClockTime />: <Navigate to="/" replace /> } />
       </Routes>
     </HashRouter>
   )
--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import users from "../assets/emps.json";
 
-function LoginForm() {
+function LoginForm( {setLogStatus} ) {
     const navigate = useNavigate();
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
@@ -15,27 +15,31 @@ function LoginForm() {
         );
 
         if (user) {
-            navigate("/React/clock", { state: { user } });
+            setLogStatus(true);
+            navigate("/React/clock", { state: { user }});
         } else {
             alert("Invalid Login");
         }
     }
 
     return (
-        <form className="outerForm" onSubmit={handleSubmit}>
-            <div className="inner-form">
-                <h2 className="LogTitle">Login</h2>
-                <div className="formGroup">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" id="name" onChange={ (e) => setName(e.target.value)}/>
+        <div className="loginDiv">
+            <img className="landingImg" src="src/assets/Images/LightHouse.jpg"/>
+            <form className="outerForm" onSubmit={handleSubmit}>
+                <div className="inner-form">
+                    <h2 className="LogTitle">Login</h2>
+                    <div className="formGroup">
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" id="name" onChange={ (e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="formGroup">
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name="password" id="password" onChange={ (e) => setPassword(e.target.value)}/>
+                    </div>
+                    <input type="submit" value="Login" />
                 </div>
-                <div className="formGroup">
-                    <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" onChange={ (e) => setPassword(e.target.value)}/>
-                </div>
-                <input type="submit" value="Login" />
-            </div>
-        </form>
+            </form>
+        </div>
     )
 }
 
